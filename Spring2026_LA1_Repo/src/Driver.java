63e96d83c81ebc870ca6d02148392fbc883cbe62
Izz Abdallah <x3zoabdullah@gmail.com>
2026-02-25 01:48:27 +0400

    Add println with my name

--- Spring2026_LA1_Repo/src/Driver.java
+++ Spring2026_LA1_Repo/src/Driver.java
@@ -2,11 +2,7 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("Khubaib Alam");
-		System.out.println("Ayman Khalid");
-		System.out.println("YASER SAFAR"); //
-		System.out.println("mahmoud "); //
-		System.out.println("Ataa Mohammed Keaied"); //
+		System.out.println("Izzeldin Ahmed");
 	}
 
 }
